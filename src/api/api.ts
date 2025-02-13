b3827a91c809a33991e245511c35e12d3abc13c6
Артём Москвин <art.moskvin2002@yandex.ru>
2025-02-13 17:05:00 +0300

    Initial commit

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export function getRepos(username: string, page: number = 1) {
+    console.log(axios.VERSION);
     return axios.get(`https://api.github.com/users/${username}/repos`, {
         params: {
             per_page: 20,
